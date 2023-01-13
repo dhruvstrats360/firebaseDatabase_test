8e455522b3d66daa9e79efd5312c6d02b5d0448d
dhruvstrats360 <dhruvchhatbar@strats360.com>
2023-01-13 15:42:15 +0530

    Initial Commit

---
+++ firebaseDatabase_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firebaseDatabase_test
+//
+//  Created by Strats 360 on 13/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
