f033e9e1cc49e2945441a503e37c56b9b132b495
dhruvstrats360 <dhruvchhatbar@strats360.com>
2023-01-17 10:50:35 +0530

    Firebase realtime Database C - done R - done U - done D - done.

--- firebaseDatabase_test/ViewController.swift
+++ firebaseDatabase_test/ViewController.swift
@@ -6,13 +6,36 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
-
+    private let dataBase = Database.database().reference()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let btn = UIButton(frame: CGRect(x: 50, y: 200, width: view.frame.width - 60, height: 50))
+        btn.backgroundColor = .link
+        btn.setTitle("Add Entry", for: .normal)
+        btn.setTitleColor(.white, for: .normal)
+        view.addSubview(btn)
+        
+        btn.addTarget(self, action: #selector(addNewEntry), for: .touchUpInside)
     }
+    @objc func addNewEntry(){
+        let object:[String : Any] = [
+            "name": "Database Test2" as NSObject,
+            "entry 1": "yes"
+        ]
+        
+        // here child string("Something") is some key in our json tree.
+//        dataBase.child("Something \(Int.random(in: 0..<100))").setValue(object)
+        dataBase.child("Something").setValue(object)
+
+    }
+    
+    
+    
 
 
 }
